4a4a458e37e66f36c2c1741aed4772ad675ff22a
Raj Gaurav Gupta <149098451+rajGauravGupta2025@users.noreply.github.com>
2024-01-25 21:20:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text" , {
+    strings: ["Frontend Devloper", "Web Devloper", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
